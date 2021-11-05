a1a814ee51db3effdd427bea8e6bde883ed00fb3
yuaokuaishou <84902119+yuaokuaishou@users.noreply.github.com>
2021-11-05 15:43:03 +0800

    Update pre_facebook_prophet.py

--- traffic_prediction/pre_facebook_prophet.py
+++ traffic_prediction/pre_facebook_prophet.py
@@ -8,7 +8,7 @@ from data_read import read_data, data_prophet
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
-path = "/Users/yuao/Downloads/"
+path = ""
 file_name = "traffic_direction.csv"
 file_name_prophet = "traffic_prophet.csv"
 file_name_prophet_results = "traffic_prophet_results.csv"
